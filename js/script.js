262daf403c3165f1165f4ee2bab68ab8e94fc168
NourNiazy <nourmostafa01551@gmail.com>
2021-01-08 13:02:19 +0200

    ...

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ generate.addEventListener("click",function GenerateButton()
    
  for(var i=0;i<number;i++)
     {
-  var div = document.createElement("div");
-  div.className="col-md-3";
-  document.getElementById("main").appendChild(div);
+      var div = document.createElement("div");
+      div.className="col-md-3";
+      document.getElementById("main").appendChild(div);
 
   }
 
