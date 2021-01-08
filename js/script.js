4cf9f10814dbfddbd7384512bef69d699f7972a3
NourNiazy <nourmostafa01551@gmail.com>
2021-01-08 12:45:31 +0200

    ...

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var letters=document.getElementById("letters");
+var generate=document.getElementById("generate");
+
+generate.addEventListener("click",function GenerateButton()
+ {
+     var number=document.getElementById("number").value;
+   
+ for(var i=0;i<number;i++)
+    {
+  var div = document.createElement("div");
+  div.className="col-md-3";
+  document.getElementById("main").appendChild(div);
+
+  }
+
+})
