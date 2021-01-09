3d43566549777b2e016ba6d56d2898f9a9dea571
NourNiazy <nourmostafa01551@gmail.com>
2021-01-09 14:52:02 +0200

    final

--- js/script.js
+++ js/script.js
@@ -10,20 +10,21 @@ generate.addEventListener("click",function GenerateButton(e)
     {
       var div = document.createElement("div");
       div.className="col-md-3";
+
       document.getElementById("main").appendChild(div);
       var x=div.id=i;
       if(i===xbox){
         var gift = document.createElement("img");
         gift.classList.add("img-style");
-        gift.src  = '../img/gift-box.png'
+        gift.src  = '../img/WhatsApp Image 2021-01-09 at 10.35.01 AM.jpeg'
         div.appendChild(gift);
       }
       div.addEventListener("click",function SrearchGift(e){
         if(e.target.id == xbox)
     {
       document.querySelector(".img-style").style.visibility="visible";
-      var statusBar = document.querySelector('.result');
-      statusBar.textContent ="You Won after "+ counter +" Steps";
+      document.querySelector('.result').innerHTML="You Won after"+"<br>"+ counter +"<br>"+ " Steps ";
+     
     }
     else
     counter++;
